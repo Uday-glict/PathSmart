debd6991eb09e32743dfdacef8cfd82f81d210cc
Uday-glict <84915848+Uday-glict@users.noreply.github.com>
2025-11-11 15:19:34 +0530

    cors handle

--- Backend/index.js
+++ Backend/index.js
@@ -24,9 +24,9 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:3000",
     "http://192.168.1.100:3000",
-    "https://path-smart-66y8.vercel.app/",
-    "https://path-smart-ui.vercel.app/",
-    "https://path-smart.vercel.app/"
+    "https://path-smart-66y8.vercel.app",
+    "https://path-smart-ui.vercel.app",
+    "https://path-smart.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
